b28863269008116f6849598a039c10c655acb7aa
Austin <austin@maddoxbox.com>
2014-06-29 17:43:40 -0600

    Simpleton

---
+++ index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<link type="text/css" rel="stylesheet" href="./css/style.css" />
+	<meta name="description" content="MaddoxBox.com is a domain reserved for the development, evolution, and progression of Austin Maddox: Boise, Idaho." />
+	<title>MaddoxBox.com</title>
+</head>
+<body>
+	<section>
+		<img src="images/maddoxbox.jpg" alt="MaddoxBox.com" />
+		<h1>MaddoxBox.com</h1>
+		<h2>A domain reserved for the development, evolution, and progression of <a href="mailto:austin@maddoxbox.com" title="Send email to Austin">Austin Maddox</a> Boise, Idaho USA.</h2>
+	</section>
+</body>
+</html>
